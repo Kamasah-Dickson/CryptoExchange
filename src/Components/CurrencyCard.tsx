83fe83351ac499e1219c3585d62df0d7c6b0eaa4
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 01:07:56 +0000

    add components

---
+++ src/Components/CurrencyCard.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Exchange from "./Exchange";
+
+function CurrencyCard() {
+	const [currencies, setCurrencies] = useState(["hello"]);
+	const [activeTab, setActiveTab] = useState(1);
+	return (
+		<form className="mx-auto mt-20 max-w-[680px]  overflow-hidden rounded-3xl bg-[white] ">
+			<div>
+				{/* ====tab header==== */}
+				<div className=" flex items-center justify-between">
+					<div
+						onClick={() => setActiveTab(2)}
+						className={`skewed-border ${
+							activeTab === 1 && "active"
+						} flex-1 cursor-pointer p-5 text-center text-xl font-normal`}
+					>
+						<h2 className={activeTab === 1 ? "skew-x-[15deg]" : ""}>
+							Crypto Exchange
+						</h2>
+					</div>
+					<div
+						onClick={() => setActiveTab(1)}
+						className={`skewed-border ${
+							activeTab === 2 && "active2"
+						} flex-1 cursor-pointer p-5 text-center  text-xl font-normal`}
+					>
+						<h2 className={activeTab === 2 ? "skew-x-[-15deg]" : ""}>
+							Buy/Sell Crypto
+						</h2>
+					</div>
+				</div>
+				{/* ==tabs==== */}
+
+				<div className="mx-auto mt-10 max-w-[600px]">
+					{activeTab === 2 ? (
+						<div className="flex flex-col items-center justify-center gap-10">
+							<Exchange currencies={currencies} amount={1} currency={"USD"} />
+							<Exchange currencies={currencies} amount={1} currency={"USD"} />
+						</div>
+					) : (
+						<div></div>
+					)}
+				</div>
+				<div className="py-10">
+					<input
+						type="submit"
+						value="Exchange"
+						className="font-largeYYYYYY mx-auto mt-4 block w-full max-w-[600px] cursor-pointer  rounded-md bg-[#370b97f5] py-5 font-small text-white active:scale-[1.06]"
+					/>
+				</div>
+			</div>
+		</form>
+	);
+}
+
+export default CurrencyCard;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+	return (
+		<div className="relative top-0 left-0 w-full border-b-2 border-[#370b9785] bg-[#ffffff10] py-5 backdrop-blur-3xl">
+			<div className="my-max flex items-center justify-between">
+				<Link to="/" className="text-2xl text-white">
+					Logo
+				</Link>
+				<ul className="flex items-center justify-center gap-7 text-white">
+					<Link
+						to="/login"
+						className="cursor-pointer rounded-sm bg-[#6f33df5e] py-2 px-5 transition-all active:scale-[1.05]"
+					>
+						Log In
+					</Link>
+					<Link
+						to="/signup"
+						className=" cursor-pointer rounded-sm bg-[#5904f5] py-2 px-3 transition-all active:scale-[1.05]"
+					>
+						Get an account
+					</Link>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
