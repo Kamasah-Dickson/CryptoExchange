2f5c2b134e9001e9d7a75f47a66a20fff1145907
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 01:15:05 +0000

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Router>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+			</Routes>
+		</Router>
 	</React.StrictMode>
 );
