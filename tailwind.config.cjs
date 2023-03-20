470380e1ea7fae2fd97eb88cbe529bed837f9a7b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 01:13:51 +0000

    update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontWeight: {
+				thin: 300,
+				small: 400,
+				large: 700,
+			},
+		},
 	},
 	plugins: [],
 };
