8764e10281c6f71bea0c9d5ec27d27d031266545
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 01:33:47 +0000

    update currencyCard

--- src/Components/CurrencyCard.tsx
+++ src/Components/CurrencyCard.tsx
@@ -5,7 +5,7 @@ function CurrencyCard() {
 	const [currencies, setCurrencies] = useState(["hello"]);
 	const [activeTab, setActiveTab] = useState(1);
 	return (
-		<form className="mx-auto mt-20 max-w-[680px]  overflow-hidden rounded-3xl bg-[white] ">
+		<form className=" my-max2 mt-20 overflow-hidden  rounded-3xl bg-[white] shadow-2xl ">
 			<div>
 				{/* ====tab header==== */}
 				<div className=" flex items-center justify-between">
@@ -13,7 +13,7 @@ function CurrencyCard() {
 						onClick={() => setActiveTab(2)}
 						className={`skewed-border ${
 							activeTab === 1 && "active"
-						} flex-1 cursor-pointer p-5 text-center text-xl font-normal`}
+						} flex-1 cursor-pointer p-4 text-center text-xl font-normal`}
 					>
 						<h2 className={activeTab === 1 ? "skew-x-[15deg]" : ""}>
 							Crypto Exchange
@@ -23,7 +23,7 @@ function CurrencyCard() {
 						onClick={() => setActiveTab(1)}
 						className={`skewed-border ${
 							activeTab === 2 && "active2"
-						} flex-1 cursor-pointer p-5 text-center  text-xl font-normal`}
+						} flex-1 cursor-pointer p-4 text-center  text-xl font-normal`}
 					>
 						<h2 className={activeTab === 2 ? "skew-x-[-15deg]" : ""}>
 							Buy/Sell Crypto
@@ -36,17 +36,16 @@ function CurrencyCard() {
 					{activeTab === 2 ? (
 						<div className="flex flex-col items-center justify-center gap-10">
 							<Exchange currencies={currencies} amount={1} currency={"USD"} />
-							<Exchange currencies={currencies} amount={1} currency={"USD"} />
 						</div>
 					) : (
 						<div></div>
 					)}
 				</div>
-				<div className="py-10">
+				<div className="py-10 px-4">
 					<input
 						type="submit"
 						value="Exchange"
-						className="font-largeYYYYYY mx-auto mt-4 block w-full max-w-[600px] cursor-pointer  rounded-md bg-[#370b97f5] py-5 font-small text-white active:scale-[1.06]"
+						className="font-largeYYYYYY mx-auto mt-4 block w-full max-w-[600px] cursor-pointer rounded-md bg-[#370b97c9] py-5 font-small text-white transition-all hover:bg-[#370b97] active:scale-[1.06]"
 					/>
 				</div>
 			</div>
