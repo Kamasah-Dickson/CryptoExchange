79ff15bf1d1dda0399075d6726aa64329b6b95f3
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 16:45:59 +0000

    update you get

--- src/Components/YouGet.tsx
+++ src/Components/YouGet.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 interface exchangeProp {
 	currencies: string[];
 	amount: number;
@@ -8,20 +7,29 @@ interface exchangeProp {
 function YouGet({ currencies, amount, currency }: exchangeProp) {
 	return (
 		<div className="flex w-full items-center justify-between gap-1">
-			<div className=" relative flex h-[60px] flex-[2] items-center justify-between">
+			<div className=" relative flex h-[55px] flex-[2] items-center justify-between">
 				<div className="absolute pl-[30px] font-small text-[#525151]">
 					You Get
 				</div>
+
 				<input
 					dir="rtl"
 					inputMode="decimal"
 					type="text"
-					className=" h-full w-full rounded-lg bg-[rgb(227,233,240)]  px-2 pl-[110px] outline-1 outline-[#8981b69d]"
+					className=" h-full w-full rounded-l-md  bg-[#8aa0c03f] px-2 pl-[110px] text-2xl outline-1 outline-[#604adbaf] focus:bg-[rgba(70,46,124,0.13)]"
 				/>
 			</div>
-			<select name="" id="" className="flex-1">
-				{currencies.map((currency) => {
-					return <option value="currency">{currency}</option>;
+			<select
+				name="userOption"
+				id=""
+				className="h-[55px] flex-1 cursor-pointer rounded-r-md bg-[#8aa0c063] p-1 font-bold uppercase text-black outline-none transition-all hover:bg-[#370b97] hover:text-white"
+			>
+				{currencies.map((currency, index) => {
+					return (
+						<option key={index} value="currency">
+							{currency}
+						</option>
+					);
 				})}
 			</select>
 		</div>
