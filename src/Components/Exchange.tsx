d1e94baf11601418c7ab71e6dd6b85f97f6d0a37
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 16:45:38 +0000

    update currency exchange

--- src/Components/Exchange.tsx
+++ src/Components/Exchange.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface exchangeProp {
 	currencies: string[];
@@ -7,21 +7,38 @@ interface exchangeProp {
 }
 
 function Exchange({ currencies, amount, currency }: exchangeProp) {
+	const [userData, setUserData] = useState({});
+
+	function updateUserOptions(e: React.ChangeEvent<HTMLInputElement>) {
+		setUserData((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+
+		console.log(userData);
+	}
+
 	return (
 		<div className="flex w-full items-center justify-between gap-1">
-			<div className=" relative flex h-[60px] flex-[2] items-center justify-between">
+			<div className=" relative flex h-[55px] flex-[2] items-center justify-between">
 				<div className="absolute pl-[30px] font-small text-[#525151]">
 					You send
 				</div>
 				<input
+					name="userCoins"
+					onChange={(e) => updateUserOptions(e)}
 					dir="rtl"
 					inputMode="decimal"
 					type="text"
-					className=" h-full w-full rounded-lg bg-[rgb(227,233,240)]  px-2 pl-[110px] outline-1 outline-[#8981b69d]"
+					className=" h-full w-full rounded-l-md
+					bg-[#8aa0c03f] px-2 pl-[110px] text-2xl outline-1 outline-[#604adbaf] focus:bg-[rgba(70,46,124,0.13)]"
 				/>
 			</div>
-			<select name="" id="" className="flex-1">
-				{currencies.map((currency) => {
+			<select
+				name="userGet"
+				className="h-[55px] flex-1 cursor-pointer rounded-r-md bg-[#8aa0c063]  p-1  font-bold uppercase text-black outline-none transition-all hover:bg-[#370b97] hover:text-white"
+			>
+				{currencies.map((currency, index) => {
 					return <option value="currency">{currency}</option>;
 				})}
 			</select>
