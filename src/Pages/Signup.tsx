27a09785022d2a2893d556a0ff005368b27b870b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 01:11:30 +0000

    add signup

---
+++ src/Pages/Signup.tsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header";
+
+function Signup() {
+	return (
+		<>
+			<Header />
+			<div className="mt-10 flex flex-col items-center justify-center leading-10 text-white">
+				<h1 className="text-6xl font-large">Crypto Exchange</h1>
+				<p>Your number one trusted friend</p>
+			</div>
+		</>
+	);
+}
+
+export default Signup;
