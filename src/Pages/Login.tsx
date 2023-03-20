a95281c3642422ca34362ce29d33147eea0f2910
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 01:10:55 +0000

    add pages

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Components/Header";
+
+function Login() {
+	return (
+		<>
+			<Header />
+			<div className="mt-10 flex flex-col items-center justify-center leading-10 text-white">
+				<h1 className="text-6xl font-large">Crypto Exchange</h1>
+				<p>Your number one trusted friend</p>
+			</div>
+		</>
+	);
+}
+
+export default Login;
