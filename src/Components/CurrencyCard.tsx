d553537536e57d058f9c78be5cbd54c24ab0d41b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 16:45:09 +0000

    update currency card

--- src/Components/CurrencyCard.tsx
+++ src/Components/CurrencyCard.tsx
@@ -3,20 +3,24 @@ import Exchange from "./Exchange";
 import YouGet from "./YouGet";
 
 function CurrencyCard() {
-	const [currencies, setCurrencies] = useState(["hello"]);
+	const [currencies, setCurrencies] = useState(["btc", "eth", "ltc"]);
 	const [activeTab, setActiveTab] = useState(1);
 	return (
-		<form className=" my-max2 mt-20 overflow-hidden  rounded-3xl bg-[white] shadow-2xl ">
-			<div>
+		<form className=" my-max2 mt-10 overflow-hidden rounded-3xl bg-white shadow-2xl ">
+			<div className="justify-cente flex flex-col">
 				{/* ====tab header==== */}
-				<div className=" flex items-center justify-between">
+				<div className=" flex items-center justify-between bg-transparent">
 					<div
 						onClick={() => setActiveTab(2)}
 						className={`skewed-border ${
 							activeTab === 1 && "active"
-						} flex-1 cursor-pointer p-4 text-center text-xl font-normal`}
+						} flex-1 cursor-pointer p-5 `}
 					>
-						<h2 className={activeTab === 1 ? "skew-x-[15deg]" : ""}>
+						<h2
+							className={`${
+								activeTab === 1 ? "skew-x-[15deg]" : ""
+							} text-center text-[15px] font-normal`}
+						>
 							Crypto Exchange
 						</h2>
 					</div>
@@ -24,31 +28,36 @@ function CurrencyCard() {
 						onClick={() => setActiveTab(1)}
 						className={`skewed-border ${
 							activeTab === 2 && "active2"
-						} flex-1 cursor-pointer p-4 text-center  text-xl font-normal`}
+						} flex-1 cursor-pointer p-5 `}
 					>
-						<h2 className={activeTab === 2 ? "skew-x-[-15deg]" : ""}>
+						<h2
+							className={`${
+								activeTab === 2 ? "skew-x-[-15deg]" : ""
+							} text-center  text-[15px] font-normal `}
+						>
 							Buy/Sell Crypto
 						</h2>
 					</div>
 				</div>
 				{/* ==tabs==== */}
-
-				<div className="mx-auto mt-10 max-w-[600px]">
-					{activeTab === 2 ? (
-						<div className="flex flex-col items-center justify-center gap-10">
-							<Exchange currencies={currencies} amount={1} currency={"USD"} />
-							<YouGet currencies={currencies} amount={1} currency={"USD"} />
-						</div>
-					) : (
-						<div></div>
-					)}
-				</div>
-				<div className="py-10 px-4">
-					<input
-						type="submit"
-						value="Exchange"
-						className="font-largeYYYYYY mx-auto mt-4 block w-full max-w-[600px] cursor-pointer rounded-md bg-[#370b97c9] py-5 font-small text-white transition-all hover:bg-[#370b97] active:scale-[1.06]"
-					/>
+				<div className="px-10">
+					<div className="mx-auto mt-10">
+						{activeTab === 2 ? (
+							<div className="flex flex-col items-center justify-center gap-7">
+								<Exchange currencies={currencies} amount={1} currency={"USD"} />
+								<YouGet currencies={currencies} amount={1} currency={"USD"} />
+							</div>
+						) : (
+							<div></div>
+						)}
+					</div>
+					<div className="pb-10 pt-5">
+						<input
+							type="submit"
+							value="Exchange"
+							className="mx-auto mt-4 block w-full cursor-pointer rounded-md bg-[#370b97] py-5 font-normal text-white transition-all hover:bg-[#370b97c9] active:scale-[1.06]"
+						/>
+					</div>
 				</div>
 			</div>
 		</form>
