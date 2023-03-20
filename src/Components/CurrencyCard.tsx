d616122303bf9fde7268b9d1c98d94b3860cfc6a
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 23:36:21 +0000

    update currency card

--- src/Components/CurrencyCard.tsx
+++ src/Components/CurrencyCard.tsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
-import Exchange from "./Exchange";
+import { useState, useEffect } from "react";
+import YouSend from "./YouSend";
 import YouGet from "./YouGet";
 
+import axios from "axios";
+
 function CurrencyCard() {
-	const [currencies, setCurrencies] = useState(["btc", "eth", "ltc"]);
-	const [activeTab, setActiveTab] = useState(1);
+	const [currencies, setCurrencies] = useState([
+		{
+			btc: 0.12312,
+			usd: 0.21254,
+			eth: 0.75234,
+			ltc: 0.3436,
+			xmr: 2.34523,
+			poof: 7.53453,
+			loom: 0.43434,
+			volt: 4.33554,
+		},
+	]);
+
+	const [userCurrency, setUserCurrency] = useState("btc");
+	const [responseCurrency, setResponseCurrency] = useState("eth");
+
+	const [activeTab, setActiveTab] = useState(2);
+	const [userCoins, setUserCoins] = useState(0.2);
+	const [responseCoins, setResponseCoins] = useState(0.0123);
+
 	return (
 		<form className=" my-max2 mt-10 overflow-hidden rounded-3xl bg-white shadow-2xl ">
 			<div className="justify-cente flex flex-col">
@@ -19,7 +39,7 @@ function CurrencyCard() {
 						<h2
 							className={`${
 								activeTab === 1 ? "skew-x-[15deg]" : ""
-							} text-center text-[15px] font-normal`}
+							} text-center text-[15px] font-medium`}
 						>
 							Crypto Exchange
 						</h2>
@@ -33,7 +53,7 @@ function CurrencyCard() {
 						<h2
 							className={`${
 								activeTab === 2 ? "skew-x-[-15deg]" : ""
-							} text-center  text-[15px] font-normal `}
+							} text-center  text-[15px] font-medium `}
 						>
 							Buy/Sell Crypto
 						</h2>
@@ -44,8 +64,29 @@ function CurrencyCard() {
 					<div className="mx-auto mt-10">
 						{activeTab === 2 ? (
 							<div className="flex flex-col items-center justify-center gap-7">
-								<Exchange currencies={currencies} amount={1} currency={"USD"} />
-								<YouGet currencies={currencies} amount={1} currency={"USD"} />
+								<YouSend
+									userCurrency={userCurrency}
+									responseCurrency={responseCurrency}
+									currencies={Object.keys(currencies[0])}
+									setUserCurrency={setUserCurrency}
+									setResponseCurrency={setResponseCurrency}
+									responseCoins={responseCoins}
+									fullCurrencies={currencies}
+									userCoins={userCoins}
+									setUserCoins={setUserCoins}
+									setResponseCoins={setResponseCoins}
+								/>
+								<YouGet
+									responseCoins={responseCoins}
+									currencies={Object.keys(currencies[0])}
+									setResponseCoins={setResponseCoins}
+									setResponseCurrency={setResponseCurrency}
+									fullCurrencies={currencies}
+									userCurrency={userCurrency}
+									responseCurrency={responseCurrency}
+									userCoins={userCoins}
+									setUserCoins={setUserCoins}
+								/>
 							</div>
 						) : (
 							<div></div>
