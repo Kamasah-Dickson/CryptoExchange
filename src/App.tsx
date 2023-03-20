6291d1c2342fb01293e36b1bd68e093d69119b54
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-20 01:14:36 +0000

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import CurrencyCard from "./Components/CurrencyCard";
+import Exchange from "./Components/CurrencyCard";
+import Header from "./Components/Header";
+
 function App() {
-	return <p className="text-3xl text-red-600">hello</p>;
+	return (
+		<>
+			<Header />
+			<div className="mt-10 flex flex-col items-center justify-center leading-10 text-white">
+				<h1 className="text-6xl font-large">Crypto Exchange</h1>
+				{/* <p>Your number one trusted friend</p> */}
+			</div>
+			<CurrencyCard />
+		</>
+	);
 }
 
 export default App;
