b72f3e3a777e5dbf837bb0d4b8e59bbbf8cc26a4
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-22 03:20:15 +0000

    update exchange

--- src/Pages/Exchange.tsx
+++ src/Pages/Exchange.tsx
@@ -5,7 +5,7 @@ import YouGet from "../Components/YouGet";
 
 import EnterWalletAddress from "../Components/EnterWalletAddress";
 import Header from "../Components/Header";
-import { VerifyWalletProvider } from "../context/contexts";
+import AdditionalInfo from "../Components/AdditionalInfo";
 
 function Exchange() {
 	const [currencies, setCurrencies] = useState([
@@ -24,7 +24,6 @@ function Exchange() {
 	const [userCurrency, setUserCurrency] = useState("btc");
 	const [responseCurrency, setResponseCurrency] = useState("eth");
 
-	const [activeTab, setActiveTab] = useState(2);
 	const [userCoins, setUserCoins] = useState(0.1);
 	const [responseCoins, setResponseCoins] = useState(0.0123);
 
@@ -34,17 +33,12 @@ function Exchange() {
 	return (
 		<>
 			<Header />
-			<div className="my-max mx-auto mt-5 bg-[#ffffffa4] p-10">
+			<div className="mx-auto my-5 ">
 				<form
 					onSubmit={(e) => handleSubmit(e)}
-					className=" my-max2 mt-10 overflow-hidden rounded-3xl bg-white shadow-2xl"
+					className=" my-max2 my-scrollbar mt-10  overflow-y-scroll rounded-2xl  bg-white shadow-2xl"
 				>
-					<div className="justify-cente flex flex-col">
-						{/* ====tab header==== */}
-						<div className="py-2 text-center">
-							<h2 className="text-xl md:text-2xl">Crypto Exchange</h2>
-						</div>
-						{/* ==tabs==== */}
+					<div className="flex flex-col justify-center">
 						<div className="px-10 pb-10">
 							<div className="mx-auto mt-10">
 								<div className="flex flex-col items-center justify-center gap-7">
@@ -72,6 +66,7 @@ function Exchange() {
 								</div>
 							</div>
 							<EnterWalletAddress />
+							<AdditionalInfo />
 						</div>
 					</div>
 				</form>
