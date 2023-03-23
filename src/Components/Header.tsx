e6124cd9e99d1df34639cd27549d6f1a29a31f72
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-23 11:45:35 +0000

    testing

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
 		<div className="sticky top-0 left-0 z-20 w-full border-b-2 border-[#370b9785] bg-[#ffffff10] py-5 backdrop-blur-lg backdrop-saturate-100">
 			<div className="my-max flex items-center justify-between">
 				<Link to="/" className="text-2xl text-white">
-					Logo is here
+					Logo
 				</Link>
 				<ul className="flex items-center justify-center gap-7 text-white">
 					<Link
