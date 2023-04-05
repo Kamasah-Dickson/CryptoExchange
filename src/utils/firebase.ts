6c25eee1f23376aa2c926ba1066ed2bfef18e6c1
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-05 01:27:32 +0000

    add utils

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyA8guWVoIklafmBKsONKo9RVKjGam3GsNs",
+	authDomain: "crypto-exchange-beef6.firebaseapp.com",
+	projectId: "crypto-exchange-beef6",
+	storageBucket: "crypto-exchange-beef6.appspot.com",
+	messagingSenderId: "668659030874",
+	appId: "1:668659030874:web:419cad897e61273fcaf163",
+	measurementId: "G-KQM7SD9XG2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
