dbd4a40974a1480f5a4b66de402993a35dec600b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-11 18:13:50 +0000

    add sidebar in dashboard

---
+++ src/Components/Dashboard_Sidebar.tsx
@@ -0,0 +1,92 @@
+import { useContext } from "react";
+import { data } from "../utils/Dashboard-data/dashboard-data";
+import { Link } from "react-router-dom";
+import { TbSettingsFilled, TbLayoutSidebarLeftCollapse } from "react-icons/tb";
+import { CgLogOut } from "react-icons/cg";
+import { useLocation } from "react-router-dom";
+import { currencyNameContext } from "../contexts/contexts";
+
+function Dashboard_Sidebar() {
+	const location = useLocation();
+	const { hideDashboard, setHideDashboard } = useContext(currencyNameContext);
+
+	return (
+		<div
+			className={`${
+				hideDashboard
+					? " absolute -translate-x-full"
+					: " relative z-20 translate-x-0 "
+			}   h-screen flex-[1] bg-[#1c1e28] transition-transform md:flex-[3] xl:flex-[2]`}
+		>
+			<h1 className="hidden p-3 px-4 pt-3 text-center text-xl text-white md:flex">
+				Lorem logo
+			</h1>
+
+			<ul className="flex h-[90%] flex-col justify-between p-3 px-4 ">
+				<div>
+					<p
+						onClick={() => setHideDashboard(true)}
+						className="
+						flex w-fit cursor-pointer items-center justify-start gap-2 rounded-lg p-3 text-[#74777e] transition-all hover:bg-gray-500 hover:text-white"
+					>
+						<TbLayoutSidebarLeftCollapse size={20} />
+					</p>
+					{data.map((data) => {
+						return (
+							<Link to={`/dashboard/${data.pathname}`} key={data.name}>
+								<li
+									className={`${
+										location.pathname === `/dashboard/${data.pathname}`
+											? "bg-[#5160f9] text-white"
+											: "text-[#74777e]"
+									} my-3 flex items-center justify-start gap-2 rounded-lg p-3 transition-all hover:bg-[#5160f9] hover:text-white`}
+								>
+									{data.icon}
+									<p className=" text-md hidden font-medium capitalize md:flex">
+										{data.name}
+									</p>
+								</li>
+							</Link>
+						);
+					})}
+				</div>
+				<div>
+					<Link to="/dashboard/settings">
+						<li
+							className={`
+						${
+							location.pathname === "/dashboard/settings"
+								? "bg-[#5160f9] text-white"
+								: "text-[#74777e]"
+						}
+						flex items-center justify-start gap-2 rounded-lg p-3 text-[#74777e] transition-all hover:bg-[#5160f9]  hover:text-white`}
+						>
+							<TbSettingsFilled size={20} />
+							<p className=" text-md hidden font-medium capitalize md:flex">
+								Settings
+							</p>
+						</li>
+					</Link>
+					<Link to="/dashboard/logout">
+						<li
+							className={`
+						${
+							location.pathname === "/dashboard/logout"
+								? "bg-[#5160f9] text-white"
+								: "text-[#74777e]"
+						}
+						flex items-center justify-start gap-2 rounded-lg p-3 text-[#74777e] transition-all hover:bg-[#5160f9] hover:text-white`}
+						>
+							<CgLogOut size={20} />
+							<p className=" text-md hidden font-medium capitalize md:flex">
+								Logout
+							</p>
+						</li>
+					</Link>
+				</div>
+			</ul>
+		</div>
+	);
+}
+
+export default Dashboard_Sidebar;
