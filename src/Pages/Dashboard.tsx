58e6fd50dbe91b5f4cabdc90c76218df396bc1cb
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-11 18:10:28 +0000

    add Dashboard

---
+++ src/Pages/Dashboard.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import Dashboard_Header from "../Components/Dashboard_Header";
+import Dashboard_Sidebar from "../Components/Dashboard_Sidebar";
+
+function Dashboard() {
+	useEffect(() => {
+		document.body.style.background = "#323440";
+	}, []);
+	return (
+		<div className=" flex w-full">
+			<Dashboard_Sidebar />
+			<div className="h-screen flex-[11] bg-[#323440] md:flex-[10]">
+				<Dashboard_Header />
+			</div>
+		</div>
+	);
+}
+
+export default Dashboard;
