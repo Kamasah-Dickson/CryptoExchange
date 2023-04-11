059aaef611e6d149f9ff0d4cf9b085ea26594616
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-11 18:12:46 +0000

    add chart

---
+++ src/Components/Chart.tsx
@@ -0,0 +1,15 @@
+import Dashboard_Sidebar from "./Dashboard_Sidebar";
+import Dashboard_Header from "./Dashboard_Header";
+
+function Chart() {
+	return (
+		<div className=" flex w-full">
+			<Dashboard_Sidebar />
+			<div className="flex-[10] bg-[#323440]">
+				<Dashboard_Header />
+			</div>
+		</div>
+	);
+}
+
+export default Chart;
